1aaf457544cf0ece8c0aa76b740ea739ede72538
Catherine Anscombe <canscombe@oucru.org>
2020-02-14 13:37:16 +0700

    ble
    Signed-off-by: Catherine Anscombe <canscombe@oucru.org>

--- assemble.py
+++ assemble.py
@@ -12,7 +12,7 @@ def main():
 	parser.add_argument ('sample_ID' , help = 'how you want out files to be named')
 
 	args = parser.parse_args()
-	assembly_whole_genome(args)
+	#assembly_whole_genome(args)
 	assembly_targets(args)
 
 
@@ -28,11 +28,10 @@ def assembly_whole_genome(args):
 	os.system ('samtools view -c %s_S.bam > %s_all_reads.txt'% (args.sample_ID , args.sample_ID))
 
 def assembly_targets(args):
-	os.system ('bwa mem %s/%s %s %s > %s_targets.sam '% (script_dir , targets , args.in_file_R1 , args.in_file_R2, args.sample_ID))
-	os.system ('samtools view -h -b -S %s_targets.sam > %s_targets.bam' % (args.sample_ID , args.sample_ID))
-	os.system ('samtools sort %s_targets.bam > %s_S_targets.bam' % (args.sample_ID, args.sample_ID))
+#	os.system ('bwa mem %s/%s %s %s > %s_targets.sam '% (script_dir , targets , args.in_file_R1 , args.in_file_R2, args.sample_ID))
+#	os.system ('samtools sort %s_targets.bam > %s_targets_S.bam' % (args.sample_ID, args.sample_ID))
 	# coverage at > Q30
-	os.system ('samtools depth -Q 30 %s_S_targets.bam | wc -l > %s_targets_Q30.depth.txt' % (args.sample_ID , args.sample_ID))
+	os.system ('samtools depth -Q 30 %s_targets_S.bam | wc -l > %s_targets_Q30.depth.txt' % (args.sample_ID , args.sample_ID))
 	# number of mapped reads
 	os.system ('samtools view -F 0x904 -c %s_targets_S.bam > %s_targets_mapped_reads.txt'% (args.sample_ID , args.sample_ID))
 	
