bbb6a1219f2a0071228be21646f9992d8330ac2b
Catherine Anscombe <canscombe@oucru.org>
2020-02-17 15:50:29 +0700

    assemble

--- assemble.py
+++ assemble.py
@@ -20,15 +20,15 @@ def main():
 
 	args = parser.parse_args()
 	assembly_whole_genome(args)
-	get_target_tats(args)
+	#get_target_tats(args)
 
 
 def assembly_whole_genome(args):
 	with open ('%s_Assembly_statsw.csv' % (args.sample_ID) , 'a' ) as rf:
 		
-		os.system ('bwa mem %s/%s %s %s > %s.sam '% (script_dir , reference , args.in_file_R1 , args.in_file_R2, args.sample_ID))
-		os.system ('samtools view -h -b -S %s.sam > %s.bam' % (args.sample_ID , args.sample_ID))
-		os.system ('samtools sort %s.bam > %s_S.bam' % (args.sample_ID, args.sample_ID))
+#		os.system ('bwa mem %s/%s %s %s > %s.sam '% (script_dir , reference , args.in_file_R1 , args.in_file_R2, args.sample_ID))
+#		os.system ('samtools view -h -b -S %s.sam > %s.bam' % (args.sample_ID , args.sample_ID))
+#		os.system ('samtools sort %s.bam > %s_S.bam' % (args.sample_ID, args.sample_ID))
 		# coverage at > Q30
 		coverage_Q30 = subprocess.check_output('samtools depth -Q 30 %s_S.bam | wc -l '  % (args.sample_ID ) , shell = True)
 		# number of mapped reads
@@ -37,27 +37,28 @@ def assembly_whole_genome(args):
 		total_reads = subprocess.check_output ('samtools view -c %s_S.bam '% (args.sample_ID ), shell = True)
 
 
-		os.system ('bwa mem %s/%s %s %s > %s_targets.sam '% (script_dir , targets , args.in_file_R1 , args.in_file_R2, args.sample_ID))
-		os.system ('samtools view -h -b -S %s_targets.sam > %s_targets.bam' % (args.sample_ID , args.sample_ID))
-		os.system ('samtools sort %s_targets.bam > %s_targets_S.bam' % (args.sample_ID, args.sample_ID))
+#		os.system ('bwa mem %s/%s %s %s > %s_targets.sam '% (script_dir , targets , args.in_file_R1 , args.in_file_R2, args.sample_ID))
+#		os.system ('samtools view -h -b -S %s_targets.sam > %s_targets.bam' % (args.sample_ID , args.sample_ID))
+#		os.system ('samtools sort %s_targets.bam > %s_targets_S.bam' % (args.sample_ID, args.sample_ID))
 		# coverage at > Q30
 		target_Q30_coverage =subprocess.check_output('samtools depth -Q 30 %s_targets_S.bam | wc -l ' % (args.sample_ID ) ,shell = True)
 		# number of mapped reads
 		target_mapped_reads = subprocess.check_output ('samtools view -F 0x904 -c %s_targets_S.bam '% (args.sample_ID ), shell = True)
-		os.system ('rm %s_targets.sam' % (args.sample_ID))
-		os.system ('rm %s.sam' % (args.sample_ID))
+#		os.system ('rm %s_targets.sam' % (args.sample_ID))
+#		os.system ('rm %s.sam' % (args.sample_ID))
 
 
 		pec_genome_cov = float(coverage_Q30)/genome_size
 		target_pec_genome_cov = float(target_Q30_coverage)/target_size
 		pec_mapped_reads = float(WG_mapped_reads)/int(total_reads)
 		pec_target_mapped_reads = float(target_mapped_reads)/int(total_reads)
-		writer =csv.writer(rf , delimiter = ',')
-		writer.writerow ([args.sample_ID,total_reads,WG_mapped_reads,pec_mapped_reads,genome_size,coverage_Q30,pec_genome_cov,target_mapped_reads,pec_target_mapped_reads,target_size,target_Q30_coverage,target_pec_genome_cov])
-	for line in fileinput.input(files = ['%s_Assembly_statsw.csv' % (args.sample_ID)] , inplace = True):
-		if fileinput.isfirstline():
-			print 'sample_ID,total_reads,WG_mapped_reads,pec_mapped_reads,genome_size,coverage_Q30,pec_genome_cov,target_mapped_reads,pec_target_mapped_reads,target_size,target_Q30_coverage,target_pec_genome_cov'
-		print line
+		print args.sample_ID,total_reads,WG_mapped_reads,pec_mapped_reads,genome_size,coverage_Q30,pec_genome_cov,target_mapped_reads,pec_target_mapped_reads,target_size,target_Q30_coverage,target_pec_genome_cov
+#		writer =csv.writer(rf , delimiter = ',')
+#		writer.writerow ([args.sample_ID,total_reads,WG_mapped_reads,pec_mapped_reads,genome_size,coverage_Q30,pec_genome_cov,target_mapped_reads,pec_target_mapped_reads,target_size,target_Q30_coverage,target_pec_genome_cov])
+#	for line in fileinput.input(files = ['%s_Assembly_statsw.csv' % (args.sample_ID)] , inplace = True):
+#		if fileinput.isfirstline():
+#			print 'sample_ID,total_reads,WG_mapped_reads,pec_mapped_reads,genome_size,coverage_Q30,pec_genome_cov,target_mapped_reads,pec_target_mapped_reads,target_size,target_Q30_coverage,target_pec_genome_cov'
+#		print line
 
 def  get_target_tats(args):
 	
