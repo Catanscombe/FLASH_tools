63a384432fd69922e74dcb8993832c3db10aa732
Catherine Anscombe <canscombe@oucru.org>
2020-02-17 14:04:09 +0700

    assemble

--- assemble.py
+++ assemble.py
@@ -24,8 +24,7 @@ def main():
 
 def assembly_whole_genome(args):
 	with open ('%s_Assembly_statsw.csv' % (args.sample_ID) , 'a' ) as rf:
-		writer = csv.writer (rf , delimiter = ',')
-		writer.writerow ('sample_ID' , 'total_reads' , 'WG_mapped_reads' , 'pec_mapped_reads' , 'genome_size' , 'coverage_Q30' , 'pec_genome_cov' , 'target_mapped_reads' , 'pec_target_mapped_reads' , 'target_size' , 'target_Q30_coverage' , 'target_pec_genome_cov')
+		
 #		os.system ('bwa mem %s/%s %s %s > %s.sam '% (script_dir , reference , args.in_file_R1 , args.in_file_R2, args.sample_ID))
 #		os.system ('samtools view -h -b -S %s.sam > %s.bam' % (args.sample_ID , args.sample_ID))
 #		os.system ('samtools sort %s.bam > %s_S.bam' % (args.sample_ID, args.sample_ID))
@@ -54,7 +53,10 @@ def assembly_whole_genome(args):
 		pec_target_mapped_reads = float(target_mapped_reads)/int(total_reads)
 		writer =csv.writer(rf , delimiter = ',')
 		wrtier.writerow ([args.sample_ID,total_reads,WG_mapped_reads,pec_mapped_reads,genome_size,coverage_Q30,pec_genome_cov,target_mapped_reads,pec_target_mapped_reads,target_size,target_Q30_coverage,target_pec_genome_cov])
-
+	for line in fileinput.input(files = ['%s_Assembly_statsw.csv' % (args.sample_ID)]) , inplace = True
+		if fileinput.isfirstline():
+			print 'sample_ID,total_reads,WG_mapped_reads,pec_mapped_reads,genome_size,coverage_Q30,pec_genome_cov,target_mapped_reads,pec_target_mapped_reads,target_size,target_Q30_coverage,target_pec_genome_cov'
+		print line
 
 def  get_target_tats(args):
 	
