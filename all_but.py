e8695f171ee7ebe16cab7f7b05e876295b352960
Catherine Anscombe <canscombe@oucru.org>
2020-02-18 19:25:06 +0700

    all_but.py

--- all_but.py
+++ all_but.py
@@ -92,7 +92,7 @@ def probe_pair_depth(args, probe_pos, depth_list):
 		probe_depth_list = list()
 		probe_pos_list = probe_pos[gene]
 		print probe_pos_list
-		sorted_pos = sorted(probe_pos_list)
+		#sorted_pos = sorted(probe_pos_list)
 		for i in range (0, len(sorted_pos)-1):
 			probe_1 = sorted_pos[i]
 			probe_2 = sorted_pos[i +1]
