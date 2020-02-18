9653f5633412e1cdaba1cef013ffa94bde5603d4
Catherine Anscombe <canscombe@oucru.org>
2020-02-18 13:40:56 +0700

    all_but.py

--- all_but.py
+++ all_but.py
@@ -87,7 +87,10 @@ def coverage_depth (args):
 
 def probe_pair_depth(args, probe_pos):
 	for gene in probe_pos:
-		print gene
+		probe_pos_list. = probe_pos[gene]
+		sorted_pos = sorted(probe_pos_list)
+
+		print sorted_pos
 
 
 
