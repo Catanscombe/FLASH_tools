8a5f93bd23fa6b9b4395748a2c2510c2bce29e0c
Catherine Anscombe <canscombe@oucru.org>
2020-02-18 09:30:15 +0700

    assemble.py

--- assemble.py
+++ assemble.py
@@ -52,20 +52,20 @@ def assembly_whole_genome(args):
 		target_pec_genome_cov = float(target_Q30_coverage)/target_size
 		pec_mapped_reads = float(WG_mapped_reads)/int(total_reads)
 		pec_target_mapped_reads = float(target_mapped_reads)/int(total_reads)
-<<<<<<< HEAD
+
 		to_print = map(str, [total_reads, WG_mapped_reads, pec_mapped_reads,genome_size,coverage_Q30,pec_genome_cov,target_mapped_reads,pec_target_mapped_reads,target_size,target_Q30_coverage,target_pec_genome_cov])
-=======
+
 		to_print = map(str,[total_reads, WG_mapped_reads, pec_mapped_reads,genome_size,coverage_Q30,pec_genome_cov,target_mapped_reads,pec_target_mapped_reads,target_size,target_Q30_coverage,target_pec_genome_cov])
->>>>>>> ae777d51af9ecf2d5c557365c1815a8ab481c489
+
 		to_print = [x.strip() for x in to_print]
 		print(','.join(to_print))
 		# print args.sample_ID,total_reads,WG_mapped_reads,pec_mapped_reads,genome_size,coverage_Q30,pec_genome_cov,target_mapped_reads,pec_target_mapped_reads,target_size,target_Q30_coverage,target_pec_genome_cov
-#		writer =csv.writer(rf , delimiter = ',')
-#		writer.writerow ([args.sample_ID,total_reads,WG_mapped_reads,pec_mapped_reads,genome_size,coverage_Q30,pec_genome_cov,target_mapped_reads,pec_target_mapped_reads,target_size,target_Q30_coverage,target_pec_genome_cov])
-#	for line in fileinput.input(files = ['%s_Assembly_statsw.csv' % (args.sample_ID)] , inplace = True):
-#		if fileinput.isfirstline():
-#			print 'sample_ID,total_reads,WG_mapped_reads,pec_mapped_reads,genome_size,coverage_Q30,pec_genome_cov,target_mapped_reads,pec_target_mapped_reads,target_size,target_Q30_coverage,target_pec_genome_cov'
-#		print line
+		writer =csv.writer(rf , delimiter = ',')
+		writer.writerow ([to_print])
+	for line in fileinput.input(files = ['%s_Assembly_statsw.csv' % (args.sample_ID)] , inplace = True):
+		if fileinput.isfirstline():
+			print 'sample_ID,total_reads,WG_mapped_reads,pec_mapped_reads,genome_size,coverage_Q30,pec_genome_cov,target_mapped_reads,pec_target_mapped_reads,target_size,target_Q30_coverage,target_pec_genome_cov'
+		print line
 
 def  get_target_tats(args):
 	
