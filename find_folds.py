4d61c445ad8ba48109b85fa1cce91df20b95b5b2
Catherine Anscombe <canscombe@oucru.org>
2020-02-19 13:54:48 +0700

    all_but.py

--- find_folds.py
+++ find_folds.py
@@ -77,6 +77,7 @@ def genome_fold (probe_pos, args, all_but_av):
 
 def fold_per_gene(args , fold_list , probe_pos):
  	for gene in probe_pos: 
+ 		print gene , probe_pos[gene]
  		last_probe = max(probe_pos[gene])
  		first_probe = min(probe_pos[gene])
  		print gene , first_probe , last_probe
