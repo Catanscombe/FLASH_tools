8e42320771c85207477fc243bd2b64a49385ecf7
Catherine Anscombe <canscombe@oucru.org>
2020-02-19 15:00:07 +0700

    all_but.py

--- find_folds.py
+++ find_folds.py
@@ -80,12 +80,12 @@ def fold_per_gene(args , fold_list , probe_pos):
  #		print gene , probe_pos[gene]
  		last_probe = int(max(probe_pos[gene]))
  		first_probe = int(min(probe_pos[gene]))
-# 		print gene , first_probe , last_probe
-		fold_points = fold_list[first_probe:last_probe]
-		print gene
-		max_fold = max(fold_points)
+ 		print gene , first_probe , last_probe
+#		fold_points = fold_list[first_probe:last_probe]
+#		print gene
+#		max_fold = max(fold_points)
 
-		print gene , max_fold
+#		print gene , max_fold
 
 
 
