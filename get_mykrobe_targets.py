c4c1e642579c8d77f61938c675aab8af3f19b832
Catherine Anscombe <canscombe@oucru.org>
2020-02-19 17:47:45 +0700

    find_probes.py

--- get_mykrobe_targets.py
+++ get_mykrobe_targets.py
@@ -194,8 +194,9 @@ def mykrobe_positions (gene_dic, mykrobe_gene):
 	return mykrobe_genome_pos
 		
 def mykrobe_fold (mykrobe_genome_pos , args):
+	fold_list = ()
 	fold = open('%s' %  (args.depth_file_fold), 'r') 
-	fold_list = fold.read()
+	fold_list = fold_list.append(fold.read())
 
 #	print fold_list
 	for gene in mykrobe_genome_pos:
